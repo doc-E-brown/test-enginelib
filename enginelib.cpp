f69ae539a57a28aa981af479c78c4bd507e4e6b6
Ben Johnston <docEbrown_github@protonmail.com>
2025-08-14 18:34:18 +1000

    initial commit

---
+++ enginelib.cpp
@@ -0,0 +1,8 @@
+#include "enginelib.h"
+
+#include <iostream>
+
+void exampleFunction() {
+    std::cout << "This is an example function." << std::endl;
+}
+
---
+++ enginelib.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Example function declaration
+void exampleFunction();
+
